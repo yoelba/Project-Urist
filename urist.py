357a3998d778c48e99da6a0a27b67cd230afa963
yoelba <yoelba365@gmail.com>
2016-04-05 13:23:41 -0400

    Added files via upload
    Collision detection

--- urist.py
+++ urist.py
@@ -15,6 +15,7 @@ class Dwarf:
 	image = pyglet.sprite.Sprite(dwarfPic, x = 0, y = 0)
 	def __init__(self, x, y, name):
 		global turnIndex
+		self.name = name
 		self.speed = 3
 		self.turn = False
 		self.turnTimer = self.speed
@@ -22,28 +23,32 @@ class Dwarf:
 		self.posX = y
 		self.dorfname = pyglet.text.Label(name, x=self.posX+50, y=self.posY+130, anchor_x='center', anchor_y='center')
 		self.image = pyglet.sprite.Sprite(dwarfPic, x=self.posX, y = self.posY)
-	def draw(self):
+	def update(self):
 		self.image.draw()
 		self.dorfname.draw()
 	def movement(self, symbol, modifiers):
 		global turnIndex
 		global entities
-		if symbol == key.RIGHT:
+		if symbol == key.RIGHT and not adjfill(">", self.posX, self.posY):
 			self.dorfname.x += 100
 			self.image.x += 100
 			self.turnTimer -= 1
-		elif symbol == key.UP:
+			self.posX += 100
+		elif symbol == key.UP and not adjfill("^", self.posX, self.posY):
 			self.dorfname.y += 100
 			self.image.y += 100
 			self.turnTimer -= 1
-		elif symbol == key.DOWN:
+			self.posY +=100
+		elif symbol == key.DOWN and not adjfill("v", self.posX, self.posY):
 			self.dorfname.y -= 100
 			self.image.y -= 100
 			self.turnTimer -=1
-		elif symbol == key.LEFT:
+			self.posY -= 100
+		elif symbol == key.LEFT and not adjfill("<", self.posX, self.posY):
 			self.dorfname.x -= 100
 			self.image.x -= 100
 			self.turnTimer -= 1
+			self.posX -= 100
 		if self.turnTimer == 0:
 			self.turn = False
 			self.turnTimer = self.speed
@@ -56,10 +61,11 @@ class Square:
 	def __init__(self, x, y):
 		self.posX = x
 		self.posY = y
+		self.fill = False
 		self.image = pyglet.sprite.Sprite(squarePNG, x = self.posX, y =self.posY)
-	def draw(self):
+	def update(self):
 		self.image.draw()
-	
+#------------------------------------------------	
 #Our characters today:
 entities = list()
 
@@ -74,37 +80,67 @@ for i in range(0,10):
 		squares.append(Square(100*i,100*j)) 
 		
 
-#-----------------
+#---------------------------------------------------
 
 entities[turnIndex].turn = True
 for i in range(0,len(entities)):
-	entities[i].draw()
-	print "WTF"
+	entities[i].update()
 for entity in entities:
-	entity.draw()
+	entity.update()
 
 
 
-#-------------------
+#-------------------------------------------------
+#Functions:
+def adjfill(dir, posX, posY):
+	ox = 0 #offset x
+	oy = 0 #offset y
+	if dir=="^":
+		oy = 100
+	if dir=="<":
+		ox = -100
+	if dir==">":
+		ox = 100
+	if dir=="v":
+		oy = -100
+
+	for square in squares:
+		if square.posX == posX+ox and square.posY == posY+oy:
+			if square.fill == True:
+				print "Collesion detected!"
+				return 	True
+			else:
+				print "Nothing locally found"
+
+	print "Nothing found at all!"
+	return False
+	
+
+def updateBoard():
+
+	for square in squares:
+		square.fill = False
+	for square in squares:
+		for entity in entities:
+			if entity.posX == square.posX and entity.posY == square.posY:
+				square.fill = True
+				print "Found a fill!"
+	
+
 @window.event
 def on_key_press(symbol, modifiers): #Movement update
 	entities[turnIndex].turn = True
 	window.clear()
+	updateBoard()
 	for square in squares:
-		square.draw()
+		square.update()
 	for entity in entities:
 		if entity.turn == True:
 			entity.movement(symbol, modifiers)
 			#entity.draw()
-		entity.draw()	
+		entity.update()	
 
-"""
-@window.event
-def on_draw():
-	for i in range(0,10):
-		pyglet.graphics.draw(2, pyglet.gl.GL_LINES,("v2f", (100*i, 0, 100*i, 800)))
-	for i in range(0,8):
-		pyglet.graphics.draw(2, pyglet.gl.GL_LINES,("v2f", (0, 100*i, window.width, 100*i)))
-"""
-	
+
+
+#-------------------------------------------
 pyglet.app.run()
